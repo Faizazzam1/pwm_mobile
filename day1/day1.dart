772e365a9881a662874d3091d0e18f6623e3b11c
Faiz <mfaizkhairul@gmail.com>
2025-07-29 10:25:35 +0700

    day1

---
+++ day1/day1.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Contoh penggunaan if-else
+  int angka = 7;
+
+  if (angka % 2 == 0) {
+    print("$angka adalah bilangan genap");
+  } else {
+    print("$angka adalah bilangan ganjil");
+  }
+
+  // Contoh penggunaan loop (for)
+  print("\nPerulangan angka dari 1 sampai 5:");
+  for (int i = 1; i <= 5; i++) {
+    print("Angka ke-$i");
+  }
+}
